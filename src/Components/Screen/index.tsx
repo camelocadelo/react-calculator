80c5356c78f50dd30342db7ca518942191c2f1bd
Kamilla Kenzhekhankyzy <kamilla.kenzhekhankyzy@nu.edu.kz>
2020-05-27 20:37:55 +0600

    Migrating to TypeScript

--- src/Components/Screen/index.tsx
+++ src/Components/Screen/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyledScreen } from './styles'
 
-const Screen = (props) => {
+const Screen = (props: any) => {
     return (
     <StyledScreen>{props.value}</StyledScreen>
       );
